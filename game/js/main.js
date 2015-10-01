c5a94c0558ec454e541dcb1dffcbd5c1239081b6
yury_chu <tesewar@mail.ru>
2015-10-01 20:38:06 +0400

    первые арты от Насти

--- game/js/main.js
+++ game/js/main.js
@@ -2,7 +2,7 @@ window.onload = main;
 
 function main() {
 	set_class("give_me_name", "visio");
-	set_class("general_section", "not_visio");
+	set_class("pets_finder", "not_visio");
 
 	btn = document.getElementById("btn_user_name");
 	btn.onclick = give_user_name;
@@ -18,7 +18,7 @@ function give_user_name(){
 	elem = document.getElementById("input_user_name");
 	var user_name = elem.value;
 	set_class("give_me_name", "not_visio");
-	set_class("general_section", "visio");
+	set_class("pets_finder", "visio");
 	var hello_user = document.getElementById("user_name");
 	hello_user.innerHTML = user_name;
 }
