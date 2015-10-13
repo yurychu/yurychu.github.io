8470aab92cecedd795d20c23fc030fbe521935a3
yury_chu <tesewar@mail.ru>
2015-10-13 21:49:01 +0400

    add divs

--- training/app/app.js
+++ training/app/app.js
@@ -12,5 +12,13 @@ window.onload = function(){
 
 function updateSales(responseText){
 	var salesDiv = document.getElementById("sales");
-	salesDiv.innerHTML = responseText;
+	var sales = JSON.parse(responseText);
+
+	for (var i = 0; i < sales.length; i++) {
+		var sale = sales[i];
+		var div = document.createElement("div");
+		div.setAttribute("class", "saleItem");
+		div.innerHTML = sale.name + " sold " + sale.sales + " gumballs!";
+		salesDiv.appendChild(div);
+	}
 }
